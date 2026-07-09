3ab3ab881539b4ccaeec74bd698a44a32b461999
Ashfarq <86925392+Ashfarq@users.noreply.github.com>
2026-07-09 17:38:05 +0530

    Change EC2 instance name from dimo-qa-pvt-ec2 to dimo-prod-pvt-ec2

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ module "compute" {
 
   ami           = "ami-0933f1385008d33c4"
   instance-type = "c6a.2xlarge"
-  instance-name = "dimo-qa-pvt-ec2"
+  instance-name = "dimo-prod-pvt-ec2"
   pvt-sub1      = module.network.pvt-1
   vpc-sg        = module.security.pvt-id
 
